d1bf8a4ac570109f972602cf29e0eaee7966e40f
MartyZhang <yan.q.zhang@mail.mcgill.ca>
2016-10-03 17:36:54 -0400

    incorrect pooling alg

--- Lab_1/lodepng_test.c
+++ Lab_1/lodepng_test.c
@@ -4,13 +4,33 @@
 #include <stdlib.h>
 
 
-int format(int c) {
+int rectify(int c) {
   if(c >= 127) {
     return c;
   } else {
     return 127;
   }
 
+}
+
+int pickLargest(int j, int k, int l, int m) {
+  int largest = 0;
+  if(j > largest) {
+    largest = j;
+  }
+
+  if(k > largest) {
+    largest = k;
+  }
+
+  if(l > largest) {
+    largest = l;
+  }
+
+  if(m > largest) {
+    largest = m;
+  }
+  return largest;
 }
 
 void process(char* input_filename, char* output_filename)
@@ -25,13 +45,43 @@ void process(char* input_filename, char* output_filename)
 
   // process image
   unsigned char value;
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {	
+  for (int i = 0; i < height; i+=2) {
+    for (int j = 0; j < width; j+=2) {	
+      char square_1_r = image[4*width*i + 4*j + 0];
+      char square_1_b = image[4*width*i + 4*j + 0];
+      char square_1_g = image[4*width*i + 4*j + 0];
 
-	    new_image[4*width*i + 4*j + 0] = format(image[4*width*i + 4*j + 0]); // R
-	    new_image[4*width*i + 4*j + 1] = format(image[4*width*i + 4*j + 1]); // G
-	    new_image[4*width*i + 4*j + 2] = format(image[4*width*i + 4*j + 2]); // B
-	    new_image[4*width*i + 4*j + 3] = image[4*width*i + 4*j + 3]; // A
+      char square_2_r = image[4*width*i + 4*(j+1) + 0];
+      char square_2_g = image[4*width*i + 4*(j+1) + 0];
+      char square_2_b = image[4*width*i + 4*(j+1) + 0];
+
+      char square_3_r = image[4*width*(i+1) + 4*j + 0];
+      char square_3_g = image[4*width*(i+1) + 4*j + 0];
+      char square_3_b = image[4*width*(i+1) + 4*j + 0];
+
+      char square_4_r = image[4*width*(i+1) + 4*(j+1) + 0];
+      char square_4_g = image[4*width*(i+1) + 4*(j+1) + 0];
+      char square_4_b = image[4*width*(i+1) + 4*(j+1) + 0]; 
+
+      char largest_r = pickLargest(square_1_r, square_2_r, square_3_r, square_4_r); 
+      char largest_g = pickLargest(square_1_g, square_2_g, square_3_g, square_4_g); 
+      char largest_b = pickLargest(square_1_b, square_2_b, square_3_b, square_4_b); 
+
+      new_image[4*width*i + 4*j + 0] = largest_r; //red
+      new_image[4*width*i + 4*j + 1] = largest_g; //green
+      new_image[4*width*i + 4*j + 2] = largest_b; //blue
+
+      new_image[4*width*i + 4*(j+1) + 0] = largest_r; //red
+      new_image[4*width*i + 4*(j+1) + 1] = largest_g; //green
+      new_image[4*width*i + 4*(j+1) + 2] = largest_b; //blue
+
+      new_image[4*width*(i+1) + 4*j + 0] = largest_r; //red
+      new_image[4*width*(i+1) + 4*j + 1] = largest_g; //green
+      new_image[4*width*(i+1) + 4*j + 2] = largest_b; //blue
+
+      new_image[4*width*(i+1) + 4*(j+1) + 0] = largest_r; //red
+      new_image[4*width*(i+1) + 4*(j+1) + 1] = largest_g; //green
+      new_image[4*width*(i+1) + 4*(j+1) + 2] = largest_b; //blue
     }
   }
 
