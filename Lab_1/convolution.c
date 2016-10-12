4ec91cbc723a7cf85c7bf1af4f6d4732b920e685
Michael Golfi <michael.m.golfi@gmail.com>
2016-10-12 13:39:30 -0400

    Added compiled and fixed test_equality. Fixed convolution.c

--- Lab_1/convolution.c
+++ Lab_1/convolution.c
@@ -9,7 +9,7 @@
 void process(char *input_filename, char *output_filename) {
     unsigned error;
     unsigned char *image, *new_image;
-    int width, height;
+    unsigned int width, height;
 
     error = lodepng_decode32_file(&image, &width, &height, input_filename);
 
@@ -20,40 +20,39 @@ void process(char *input_filename, char *output_filename) {
 
     new_image = malloc(new_width * new_height * 4 * sizeof(unsigned char));
     int RED = 0, GREEN = 1, BLUE = 2, ALPHA = 3;
-#pragma omp parallel for
+
+    int r = 0, g = 0, b = 0, indexR = 0, indexG = 0, indexB = 0;
+
     for (int i = 1; i < height; i++) {
-#pragma omp parallel for
         for (int j = 1; j < width; j++) {
 
-            int r = 0, g = 0, b = 0, a = 0, indexR = 0, indexG = 0, indexB = 0;
+            r = g = b = 0;
+
             for (int ii = 0; ii < weight_size; ii++) {
-#pragma omp parallel for reduction (+:r)
                 for (int jj = 0; jj < weight_size; jj++) {
-                    indexR = (int) (4 * width * (i + ii - 1) + 4 * (j + jj - 1));
+                    indexR = 4 * width * (i + ii - 1) + 4 * (j + jj - 1);
                     r += image[indexR + RED] * w[ii][jj];
                 }
             }
 
             for (int ii = 0; ii < weight_size; ii++) {
-#pragma omp parallel for reduction (+:g)
                 for (int jj = 0; jj < weight_size; jj++) {
-                    indexG = (int) (4 * width * (i + ii - 1) + 4 * (j + jj - 1));
+                    indexG = 4 * width * (i + ii - 1) + 4 * (j + jj - 1);
                     g += image[indexG + GREEN] * w[ii][jj];
                 }
             }
 
             for (int ii = 0; ii < weight_size; ii++) {
-#pragma omp parallel for reduction (+:b)
                 for (int jj = 0; jj < weight_size; jj++) {
-                    indexB = (int) (4 * width * (i + ii - 1) + 4 * (j + jj - 1));
+                    indexB = 4 * width * (i + ii - 1) + 4 * (j + jj - 1);
                     b += image[indexB + BLUE] * w[ii][jj];
                 }
             }
 
-            new_image[4 * new_width * i + 4 * j + RED] = (unsigned char) clamp(r);
-            new_image[4 * new_width * i + 4 * j + GREEN] = (unsigned char) clamp(g);
-            new_image[4 * new_width * i + 4 * j + BLUE] = (unsigned char) clamp(b);
-            new_image[4 * new_width * i + 4 * j + ALPHA] = 255;
+            new_image[4 * new_width * (i - 1) + 4 * (j - 1) + RED] = (unsigned char) clamp(r);
+            new_image[4 * new_width * (i - 1) + 4 * (j - 1) + GREEN] = (unsigned char) clamp(g);
+            new_image[4 * new_width * (i - 1) + 4 * (j - 1) + BLUE] = (unsigned char) clamp(b);
+            new_image[4 * new_width * (i - 1) + 4 * (j - 1) + ALPHA] = 255;
         }
     }
 
