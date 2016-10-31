7776d5c5008ddb6a6a87e6b26bed1898757099ce
Michael Golfi <michael.m.golfi@gmail.com>
2016-10-31 19:02:38 -0400

    Completed test run with q1

--- Lab_2/grid_4_4.py
+++ Lab_2/grid_4_4.py
@@ -3,9 +3,6 @@ from mpi4py import MPI
 import numpy
 import sys
 
-def Send(rank, intensity):
-    comm.Send(intensity, dest=rank)
-
 def NotAnEdge(rank):
     return True if (rank % 4 > 0 and rank % 4 < 3) and (rank / 4 > 0 and rank / 4 < 3) else False
 
@@ -47,20 +44,19 @@ size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
 name = MPI.Get_processor_name()
 
-position = (rank / 4, rank % 4)
-
 T = int(sys.argv[1])
-intensities = numpy.zeros(T, dtype=numpy.int)
+intensities = numpy.zeros(3, dtype=numpy.float32)
 
-top = numpy.zeros(1, dtype=numpy.int)
-left = numpy.zeros(1, dtype=numpy.int)
-right = numpy.zeros(1, dtype=numpy.int)
-bottom = numpy.zeros(1, dtype=numpy.int)
+top = numpy.zeros(1, dtype=numpy.float32)
+left = numpy.zeros(1, dtype=numpy.float32)
+right = numpy.zeros(1, dtype=numpy.float32)
+bottom = numpy.zeros(1, dtype=numpy.float32)
 
 # Constants
-n = 0.5
-p = 1
-G = 10
+n = 0.0002
+p = 0.5
+G = 0.75
+arraySize = 3
 
 def up(rank):
     return 4 * (int(rank / 4) - 1) + ((rank % 4))
@@ -69,102 +65,122 @@ def down(rank):
     return 4 * (int(rank / 4) + 1) + ((rank % 4))
 
 def leftIndex(rank):
-    return 4 * (int(rank / 4)) + ((rank % 4) - 1)
+    return rank - 1
 
 def rightIndex(rank):
-    return 4 * (int(rank / 4)) + ((rank % 4) + 1)
+    return rank + 1
+
+current = numpy.zeros(1, dtype=numpy.float32)
+previous = numpy.zeros(1, dtype=numpy.float32)
+previousPrevious = numpy.zeros(1, dtype=numpy.float32)
 
 if Center(rank):
-    intensities[0] = 1
-
-
+    current[0] = 1.0
+    previous[0] = 1.0
+    
 # Decrement to 0
-for i in range(T - 1, 0, -1):
+for i in range(0, T):
     comm.Barrier()
-
+    
     if NotAnEdge(rank):
-        Send(up(rank), intensities[T - i - 1])
-        Send(leftIndex(rank), intensities[T - i - 1])
-        Send(rightIndex(rank), intensities[T - i - 1])
-        Send(down(rank), intensities[T - i - 1])
+        if rank == 5:
+            comm.Send(previous[0], dest=rightIndex(rank))
+            comm.Send(previous[0], dest=down(rank))
+
+        if rank == 6:
+            comm.Send(previous[0], dest=leftIndex(rank))
+            comm.Send(previous[0], dest=down(rank))
+
+        if rank == 9:
+            comm.Send(previous[0], dest=up(rank))
+            comm.Send(previous[0], dest=rightIndex(rank))
+
+        if rank == 10:
+            comm.Send(previous[0], dest=up(rank))
+            comm.Send(previous[0], dest=leftIndex(rank))
 
         comm.Recv(top, source=up(rank))
         comm.Recv(left, source=leftIndex(rank))
-
         comm.Recv(right, source=rightIndex(rank))
         comm.Recv(bottom, source=down(rank))
-        #print "Rank: %d Left: %d Top: %d Right: %d Bottom: %d" % (rank, leftIndex(rank), up(rank), rightIndex(rank), down(rank))
-        
-        lastIntensity = intensities[T - i - 1] if (T - i - 1 >= 0) else 0
-        lastLastIntensity = intensities[T - i - 2] if (T - i - 2 >= 0) else 0 
 
-        intensities[T - i] = CalculateNewIntensity(top[0], left[0], right[0], bottom[0], lastIntensity, lastLastIntensity, p, n)
+        current[0] = CalculateNewIntensity(top[0], left[0], right[0], bottom[0], previous[0], previousPrevious[0], p, n)
 
-    # Calculate Send for edges
+        if rank == 5:
+            comm.Send(current[0], dest=up(rank))
+            comm.Send(current[0], dest=leftIndex(rank))
+
+        if rank == 6:
+            comm.Send(current[0], dest=up(rank))
+            comm.Send(current[0], dest=rightIndex(rank))
+
+        if rank == 9:
+            comm.Send(current[0], dest=leftIndex(rank))
+            comm.Send(current[0], dest=down(rank))
+
+        if rank == 10:
+            comm.Send(current[0], dest=rightIndex(rank))
+            comm.Send(current[0], dest=down(rank))
+
+    # Calculate comm.Send for edges
     elif IsLeftEdge(rank):
-        Send(up(rank), intensities[T - i - 1])
-        Send(rightIndex(rank), intensities[T - i - 1])
-        Send(down(rank), intensities[T - i - 1])
+        comm.Send(current[0], dest=rightIndex(rank))
 
         comm.Recv(right, source=rightIndex(rank))
-        intensities[T - i] = G * right[0]
+        current[0] = G * right[0]
+
+        comm.Send(current[0], dest=up(rank))
+        comm.Send(current[0], dest=down(rank))
 
     elif IsRightEdge(rank):
-        Send(up(rank), intensities[T - i - 1])
-        Send(leftIndex(rank), intensities[T - i - 1])
-        Send(down(rank), intensities[T - i - 1])
+        comm.Send(current[0], dest=leftIndex(rank))
 
         comm.Recv(left, source=leftIndex(rank))
-        intensities[T - i] = G * left[0]
+        current[0] = G * left[0]
+
+        comm.Send(current[0], dest=up(rank))
+        comm.Send(current[0], dest=down(rank))
 
     elif IsTopEdge(rank):
-        Send(leftIndex(rank), intensities[T - i - 1])
-        Send(rightIndex(rank), intensities[T - i - 1])
-        Send(down(rank), intensities[T - i - 1])
+        comm.Send(current[0], dest=down(rank))
 
         comm.Recv(bottom, source=down(rank))
-        intensities[T - i] = G * bottom[0]
+        current[0] = G * bottom[0]
+
+        comm.Send(current[0], dest=leftIndex(rank))
+        comm.Send(current[0], dest=rightIndex(rank))
 
     elif IsBottomEdge(rank):
-        Send(up(rank), intensities[T - i - 1])
-        Send(leftIndex(rank), intensities[T - i - 1])
-        Send(rightIndex(rank), intensities[T - i - 1])
+        comm.Send(current[0], dest=up(rank))
 
         comm.Recv(top, source=up(rank))
-        intensities[T - i] = G * top[0]
+        current[0] = G * top[0]
+
+        comm.Send(current[0], dest=leftIndex(rank))
+        comm.Send(current[0], dest=rightIndex(rank))
 
     # Must be a corner
     elif TopLeftCorner(rank):
-        Send(rightIndex(rank), intensities[T - i - 1])
-        Send(down(rank), intensities[T - i - 1])
-
         comm.Recv(right, source=rightIndex(rank))
-        intensities[T - i] = G * right[0]
+        current[0] = G * right[0]
         
     elif TopRightCorner(rank):
-        Send(leftIndex(rank), intensities[T - i - 1])
-        Send(down(rank), intensities[T - i - 1])
-
         comm.Recv(left, source=leftIndex(rank))
-        intensities[T - i] = G * left[0]
+        current[0] = G * left[0]
 
     elif BottomLeftCorner(rank):
-        Send(up(rank), intensities[T - i - 1])
-        Send(rightIndex(rank), intensities[T - i - 1])
-
         comm.Recv(top, source=up(rank))
-        intensities[T - i] = G * right[0]
+        current[0] = G * top[0]
 
     elif BottomRightCorner(rank):
-        Send(up(rank), intensities[T - i - 1])
-        Send(leftIndex(rank), intensities[T - i - 1])
-
         comm.Recv(top, source=up(rank))
-        intensities[T - i] = G * left[0]
+        current[0] = G * top[0]
+
+    if NotAnEdge(rank):
+        previousPrevious[0] = previous[0]
+        previous[0] = current[0]
 
     if Center(rank):
-        print "Rank N/2 has Intensities: %d\n" % intensities[T - i]
+        print "Rank N/2 has Intensities: %f\n" % current[0]
 
     comm.Barrier()
-
-#print "Rank %d has Intensities: %s" % (rank, str(intensities)) 
