839b0b2d37f1e86dae9a8bc3e593e255c8cafca2
Michael Golfi <michael.m.golfi@gmail.com>
2016-11-02 19:40:46 -0400

    Added scp

--- Lab_2/local.sh
+++ Lab_2/local.sh
@@ -1,2 +1,4 @@
 ip=$1
 ssh "ubuntu@$ip" -i /mnt/c/Users/micha/.ssh/id_rsa "nohup mpirun -np 8 python ~/Parallel_computing_labs/Lab_2/grid_512_512.py 2000 > ~/output.txt 2>&1 &"
+
+scp -i /mnt/c/Users/micha/.ssh/id_rsa "ubuntu@$1:/home/ubuntu/output.txt" .
